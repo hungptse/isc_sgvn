e4eacfb2e7dc5acdf141fa9a15401f37a02e8910
Thanh Hung <hungpt.se@gmail.com>
2019-12-11 13:56:35 +0700

    init project

---
+++ src/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+/*
+color options : 
+	 'light.purple'		'dark.purple'
+	 'light.blue'		  'dark.blue'
+	 'light.green'		'dark.green'
+	 'light.orange'		'dark.orange'
+	 'light.red'		  'dark.red'
+*/
+var color = 'light.red';
+if (localStorage.getItem('themeColor')) {
+  color = localStorage.getItem('themeColor');
+}
+
+let render = () => {
+  const css = import('./assets/css/sass/themes/gogo.' + color + '.scss').then(x => {
+    const MainApp = require('./App').default;
+
+    ReactDOM.render(
+      <MainApp />,
+      document.getElementById("root")
+    );
+  });
+
+};
+
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    const css = import('./assets/css/sass/themes/gogo.' + color + '.scss').then(x => {
+      const NextApp = require('./App').default;
+
+      render(
+        <NextApp />,
+        rootEl
+      );
+    });
+  });
+
+}
+
+render() 
